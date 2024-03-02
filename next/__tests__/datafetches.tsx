283c2b1042db6172c83e0a270d0f8b27fb251fec
AK-kyWatchingYou <b2220060@photon.chitose.ac.jp>
2024-03-02 21:01:31 +0900

    node_module fixed

---
+++ next/__tests__/datafetches.tsx
@@ -0,0 +1,7 @@
+import { fetchCardData } from "@/lib/functions/fetch"
+
+describe('質問のフェッチ', () => {
+    test('質問のカードをフェッチする', async () => {
+        const datas = fetchCardData()
+    })
+})
--- next/__tests__/sample.test.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactElement } from 'react';
-import { render, screen, fireEvent } from "@testing-library/react";
-import Sample from '../Sample';
-
-describe("Sample", () => {
-    it("コンポーネントのレンダリングのテスト", async () => {
-        render(<Sample />);
-    });
-});
